5ba2926a39c1483d84ba869280fdbef75ad99c70
ponchautf <frederic.ponchaut@gmail.com>
2021-02-05 15:23:37 +0100

    react to height changes

--- src/index.js
+++ src/index.js
@@ -31,6 +31,11 @@ class RBSheet extends Component {
     this.createPanResponder(props);
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+   return {
+    animatedHeight: new Animated.Value(nextProps.height)
+   };
+  }
   setModalVisible(visible, props) {
     const { height, minClosingHeight, openDuration, closeDuration, onClose, onOpen } = this.props;
     const { animatedHeight, pan } = this.state;
